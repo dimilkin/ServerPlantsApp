34db619fe35f74b7cce769440023ecd526a6b3ad
dimilkin <drmilkin@gmail.com>
2023-02-24 09:08:37 +0200

    Exceptions logging update

--- src/main/java/com/server/backend/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/com/server/backend/exceptions/GlobalExceptionsHandler.java
@@ -25,55 +25,55 @@ public class GlobalExceptionsHandler {
 
     @ExceptionHandler(value = NoResultException.class)
     public ResponseEntity<Object> handleInvalidInputException(NoResultException ex) {
-        logger.error("No info for query found: ", ex.getMessage());
+        logger.error("No data was found for query: " + ex.getMessage());
         return new ResponseEntity<Object>("No data found for your request", HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler(value = NonUniqueResultException.class)
     public ResponseEntity<Object> handleInvalidInputException(NonUniqueResultException ex) {
-        logger.error("More than one results found for query: ", ex.getMessage());
+        logger.error("More than one results found for query: " + ex.getMessage());
         return new ResponseEntity<Object>("More than one item found for your request", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = DuplicateEntityException.class)
     public ResponseEntity<Object> handleDuplicatedEntityException(DuplicateEntityException ex) {
-        logger.error("More than one results found for query: ", ex.getMessage());
+        logger.error("More than one results found for query: " + ex.getMessage());
         return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity<Object> handleInvalidInputException(EntityNotFoundException ex) {
-        logger.error("No Info found for query: ", ex.getMessage());
+        logger.error("No Info found for query: " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(value = DisabledException.class)
     public ResponseEntity<Object> handleDisabledUserException(DisabledException ex) {
-        logger.error("No Info found for query: ", ex.getMessage());
+        logger.error("User is disabled: " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.NOT_FOUND).body("No Such User Found");
     }
 
     @ExceptionHandler(value = BadCredentialsException.class)
     public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex) {
-        logger.error("No Info found for query: ", ex.getMessage());
+        logger.error("Attempted LogIn with incorrect credentials " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.UNAUTHORIZED).body("Incorrect Email or Password");
     }
 
     @ExceptionHandler(value = InternalAuthenticationServiceException.class)
     public ResponseEntity<Object> handleAuthenticationException(InternalAuthenticationServiceException ex) {
-        logger.error("Authentication Exception : ", ex.getMessage());
+        logger.error("Failed Authentication : " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.UNAUTHORIZED).body("Authentication Failed");
     }
 
     @ExceptionHandler(value = RequestRejectedException.class)
     public ResponseEntity<Object> handleAuthenticationException(RequestRejectedException ex) {
-        logger.error("Authentication Exception : ", ex.getMessage());
+        logger.error("Failed Authentication : " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.UNAUTHORIZED).body("Authentication Failed");
     }
 
     @ExceptionHandler(value = IllegalAccessException.class)
     public ResponseEntity<Object> handleBadCredentialsException(IllegalAccessException ex) {
-        logger.error("No Info found for query: ", ex.getMessage());
+        logger.error("User trying to access restricted resources : " + ex.getMessage());
         return ResponseEntity.status( HttpStatus.BAD_REQUEST).body("No permission to access these resources");
     }
 
